594932acc5ba95b1c4fdaf851603c7b3a89a2cdc
Domício Medeiros <domicio.medeiros@protonmail.com>
2022-01-12 20:21:05 +0000

    withdraw amount

--- src/main/scala/AccountServiceImpl.scala
+++ src/main/scala/AccountServiceImpl.scala
@@ -1,7 +1,7 @@
 class AccountServiceImpl(presenter: Presenter, calendar: Calendar, var account: Account) extends AccountService:
   def deposit(amount: Int): Unit = account = account.deposit(amount)
   def printStatement(): Unit = ???
-  def withdraw(amount: Int): Unit = ???
+  def withdraw(amount: Int): Unit = account = account.withdraw(amount)
 
 object AccountServiceImpl:
   def apply(presenter: Presenter, calendar: Calendar, account: Account) = new AccountServiceImpl(presenter, calendar, account)
--- src/test/scala/AccountServiceShould.scala
+++ src/test/scala/AccountServiceShould.scala
@@ -7,4 +7,11 @@ class AccountServiceShould {
     val accountService = AccountServiceImpl(MockPresenter(), Calendar(), Account())
     accountService.deposit(10)
     assertEquals(10, accountService.account.balance)
+
+  @Test
+  def withdraw() =
+    val accountService = AccountServiceImpl(MockPresenter(), Calendar(), Account())
+    accountService.deposit(10)
+    accountService.withdraw(5)
+    assertEquals(5, accountService.account.balance)
 }
