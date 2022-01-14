c14ec7b8be3180f22fe0082d05203296179b854f
Domício Medeiros <domicio.medeiros@protonmail.com>
2022-01-14 10:17:24 +0000

    ...

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -1,17 +1,15 @@
 import java.time.LocalDate
 
 class Account(statements: List[Statement], val balance: Int = 0):
-  def deposit(amount: Int, date: LocalDate): Account = {
+  def deposit(amount: Int, date: LocalDate): Account =
     val newBalance = balance + amount
     Account(statements :+ Deposit(date, amount, newBalance), newBalance)
-  }
 
   def withdraw(amount: Int, date: LocalDate): Account =
     if amount > balance then this
-    else {
+    else
       val newBalance = balance - amount
       Account(statements :+ Withdraw(date, amount, newBalance), newBalance)
-    }
 
   def getStatements(): List[Statement] = statements
 
--- src/main/scala/Calendar.scala
+++ src/main/scala/Calendar.scala
@@ -1,4 +1,5 @@
 import java.time.{LocalDate, LocalDateTime}
+
 class Calendar(var date: LocalDate) {
   def setDate(date: LocalDate) = this.date = date
   def getDate() = date
